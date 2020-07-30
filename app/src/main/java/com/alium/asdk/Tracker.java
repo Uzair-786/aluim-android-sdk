148304ea6df34fa63f69126649163d2fcebdc319
Uzair-786 <uzair.txt@gmail.com>
2020-07-30 14:26:58 +0530

    Initial Commit

---
+++ app/src/main/java/com/alium/asdk/Tracker.java
@@ -0,0 +1,64 @@
+package com.alium.asdk;
+
+import android.os.Build;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import androidx.annotation.RequiresApi;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tracker {
+
+    private static final String TAGS = "Trackers ";
+    int counter = 0;
+    static Map<String,Integer> map =new HashMap();
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void onClickTracker(View view) {
+        Log.d(TAGS, "Inside On ClickTracker()");
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+
+                    String viewName = view.getResources().getResourceEntryName(view.getId());
+                    Log.d(TAGS, "Item clicked :(name):(" + viewName + ")");
+
+                    clickCounter(viewName,counter);
+
+                    String widgetName = view.getAccessibilityClassName().toString();
+                    String[] className = widgetName.split("\\.");
+                    Log.d(TAGS, "Widget Class Name : (v): (" + className[2] + ")");
+
+                    return false;
+                }
+
+                return false;
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void clickCounter(String viewName,Integer counter){
+
+        if(!map.containsKey(viewName)){
+            map.put(viewName,counter);
+        }
+
+        for(String key : map.keySet()){
+            if(viewName.equals(key)){
+                Integer count = map.get(viewName);
+                count++;
+                map.replace(viewName,count) ;
+            }
+        }
+
+        Log.d(TAGS, "Map of Touched Items : (v): ("+map+")");
+
+    }
+
+}
